3f2af1ea2489cc008b1d0eee262a58a2ac8ec6c0
Marianna <20104120@mail.wit.ie>
2024-08-05 23:35:40 +0100

    styles and dateFormat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import { Note as NoteModel } from './models/note';
 import Note from "./components/Note";
+import styles from "./styles/NotesPage.module.css";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -23,11 +24,15 @@ function App() {
   }, []);
 
   return (
-    <div >
+    <Container >
+      <Row xs={1} md={2} xl={3} className="g-4">
       {notes.map(note => (
-        <Note note={note} key={note._id}/>
+        <Col key={note._id}>
+          <Note note={note} className={styles.note}/>
+        </Col>
       ))}
-    </div>
+      </Row>
+    </Container>
   );
 }
 
