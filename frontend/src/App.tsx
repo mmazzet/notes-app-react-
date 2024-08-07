9a5fdf1b44f057c3c7da1a5df33c1c54b18071f4
Marianna <20104120@mail.wit.ie>
2024-08-07 11:55:48 +0100

    created fetchData function and addNoteDialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,18 @@ import { Button, Container, Row, Col } from 'react-bootstrap';
 import { Note as NoteModel } from './models/note';
 import Note from "./components/Note";
 import styles from "./styles/NotesPage.module.css";
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from './components/AddNoteDialog';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
 
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", { method: "GET"});
-        const notes = await response.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -25,6 +28,9 @@ function App() {
 
   return (
     <Container >
+      <Button onClick={() => setShowAddNoteDialog(true)}>
+        Add new note
+      </Button>
       <Row xs={1} md={2} xl={3} className="g-4">
       {notes.map(note => (
         <Col key={note._id}>
@@ -32,6 +38,12 @@ function App() {
         </Col>
       ))}
       </Row>
+      { showAddNoteDialog &&
+        <AddNoteDialog 
+          onDismiss={() => setShowAddNoteDialog(false)}
+        />
+
+      }
     </Container>
   );
 }
