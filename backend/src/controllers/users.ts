aaf0e4b46c2b6ebe7d2222b3ff6ddfb6f013830c
Marianna <20104120@mail.wit.ie>
2024-08-14 23:28:58 +0100

    implemented logut and destroy session

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -3,6 +3,22 @@ import createHttpError from "http-errors";
 import UserModel from "../models/user";
 import bcrypt from "bcrypt";
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+    const authenticatedUserId = req.session.userId;
+
+    try {
+        if (!authenticatedUserId) {
+            throw createHttpError(401, "User not authenticated");
+        }
+
+        const user = await UserModel.findById(authenticatedUserId).select("+email").exec();
+        res.status(200).json(user);
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
 interface SignUpBody {
     username?: string;
     email?: string;
@@ -81,3 +97,13 @@ export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async
         next(error);
     }
 };
+
+export const logout: RequestHandler = (req, res, next) => {
+    req.session.destroy(error => {
+        if (error) {
+            next(error);
+        } else {
+            res.sendStatus(200);
+        }
+    })
+}
