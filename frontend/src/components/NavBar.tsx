2e73a9a556d75b60d7344bb5135c7befdbfd4e59
Marianna <20104120@mail.wit.ie>
2024-08-26 23:12:44 +0100

    added ar and rtl improved  dropdown menu

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { User } from "../models/user";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import NavBarLoggedInView from "./NavBarLoggedInView";
@@ -6,8 +7,7 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 import GlobeIcon from "./GlobeIcon";
-
-
+import cookie from "js-cookie";
 
 
 interface NavBarProps {
@@ -29,11 +29,23 @@ const languages = [
         name: "Italiano",
         country_code: "it"
     },
+    {
+        code: "ar",
+        name: "العربية",
+        country_code: "sa",
+        dir: "rtl"
+    },
 
 ]
 
 const NavBar = ({ loggedInUser, onSignUpClicked, onLoginClicked, onLogoutSuccessful }: NavBarProps) => {
+    
+    const currentLanguageCode = cookie.get("i18next") || "en"
+    const currentLanguage = languages.find(l => l.code === currentLanguageCode)
     const { t } = useTranslation();
+    useEffect (() => {
+        document.body.dir = currentLanguage?.dir || "ltr"
+    }, [currentLanguage])
     return (
         <Navbar bg="primary" variant="dark" expand="sm" sticky="top">
             <Container>
@@ -58,11 +70,24 @@ const NavBar = ({ loggedInUser, onSignUpClicked, onLoginClicked, onLogoutSuccess
                             <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <GlobeIcon />
                             </button>
+
                             <ul className="dropdown-menu">
+                            <li><span className="dropdown-item-text">
+                                {t("language")}
+                                </span>
+                            </li>
                                 {languages.map(({ code, name, country_code }) => (
                                     <li key={country_code}>
-                                        <button className="dropdown-item" onClick={() => i18next.changeLanguage(code)} >
-                                            <span className={`fi fi-${country_code} mx-2`}></span>
+                                        <button 
+                                        className="dropdown-item" 
+                                        onClick={() => i18next.changeLanguage(code)} 
+                                        disabled={code === currentLanguageCode}
+                                        >
+                                            <span 
+                                            className={`fi fi-${country_code} mx-2`}
+                                            style={{ opacity: code === currentLanguageCode ? 0.5 : 1}}
+                                            >
+                                            </span>
                                             {name}
                                         </button>
                                     </li>
--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -3,19 +3,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
-import React, { Suspense } from "react";
-
-
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
-    supportedLngs:["en", "it"],
+    supportedLngs:["en", "it", "ar"],
     fallbackLng: "en",
     detection: {
-        order: ['cookie','htmlTag', 'localStorage', 'path', 'subdomain'],
+        order: ['path', 'cookie','htmlTag', 'localStorage', 'subdomain'],
         caches: ['cookie'],
     },
     backend: {
